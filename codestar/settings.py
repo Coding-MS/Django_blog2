518ded4127fdb3f63a9e80f179e52d1ad8adaec2
Matthew  Smith <Matthew.smith102023@outlook.com>
2023-11-17 15:34:53 +0000

    Enable serving of static files

--- codestar/settings.py
+++ codestar/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
